1b8fc892b966c0b13cbdbebe77c78622c82ac905
BB-cpu <biancaberza@yahoo.com>
2020-11-30 15:41:42 +0200

    probleme 2

---
+++ src/lab2/Main1.java
@@ -0,0 +1,8 @@
+package lab2;
+
+public class Main {
+    public static void main(String[] args){
+        Fighter f1= new Fighter( name:"Fighter1", health:100, damagePerAttack:20)
+        Fighter f2= new Fighter( name:"Fighter2", health:100, damagePerAttack:30)
+    }
+}
